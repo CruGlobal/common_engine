9b86c56ae168df597196ce45ea3ad32b344c7035
Josh Starcher <josh.starcher@gmail.com>
2010-08-16 09:30:29 -0500

    address helper

--- app/models/address.rb
+++ app/models/address.rb
@@ -27,6 +27,17 @@ class Address < ActiveRecord::Base
 		return ret_val
 	end
 	
+	def address
+	 	ret_val = address1 || ''
+		ret_val += ', ' + address2 unless address2.nil? || address2.empty? 
+		ret_val += ', ' unless ret_val.empty?
+		ret_val += city + ', ' unless city.nil? || city.empty? 
+		ret_val += state + ' ' unless state.nil?
+		ret_val += zip unless zip.nil?
+		ret_val += ', ' + country unless country.nil? || country.empty? || country == 'USA'
+		return ret_val
+	end
+	
 	def phone_number
     phone = (self.homePhone && !self.homePhone.empty?) ? self.homePhone : self.cellPhone
     phone = (phone && !phone.empty?) ? phone : self.workPhone
