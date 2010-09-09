ed76a3477841ca85630824fe9e51abf98b014101
Josh Starcher <josh.starcher@gmail.com>
2010-09-08 19:45:49 -0500

    less required fields

--- app/models/sp_project.rb
+++ app/models/sp_project.rb
@@ -27,28 +27,24 @@ class SpProject < ActiveRecord::Base
   
 
   validates_presence_of :name, :display_location, :start_date, :end_date, :student_cost, :max_accepted_men, :max_accepted_women,
-                        :project_contact_name, :project_contact_role, :project_contact_phone, :project_contact_email, 
-                        :project_contact2_name, :project_contact2_role, :project_contact2_phone, :project_contact2_email, 
-                        :staff_start_date, :staff_end_date,
+                        :project_contact_name, 
                         :city, :country, :aoa, 
                         :primary_partner, :report_stats_to
-                        
-  validates_inclusion_of :use_provided_application, :in => [true, false]
-  validates_inclusion_of :partner_region_only, :in => [true, false]
 
-#  validates_presence_of :primary_ministry_focus_id, :description,
-#                        :operating_business_unit, :operating_operating_unit,
-#                        :operating_department, :operating_designation,
-#                        :scholarship_business_unit, :scholarship_operating_unit,
-#                        :scholarship_department, :scholarship_designation,
+# :project_contact_role, :project_contact_phone, :project_contact_email, 
+#                         :project_contact2_name, :project_contact2_role, :project_contact2_phone, :project_contact2_email, 
+#                         :staff_start_date, :staff_end_date,
+#                                                 
+  validates_inclusion_of :use_provided_application, :partner_region_only, :in => [true, false], :message => "can't be blank"
+
 #                        
   validates_presence_of  :apply_by_date, :if => :use_provided_application
   validates_presence_of  :state, :if => Proc.new { |project| !project.is_wsn? } #:is_wsn?
 
   validates_inclusion_of :max_student_men_applicants, :in=>1..100, :message => "can't be 0 or greater than 100"
   validates_inclusion_of :max_student_women_applicants, :in=>0..100, :message => "can't be less than 0 or greater than 100"
-  validates_inclusion_of :ideal_staff_men, :in=>1..100, :message => "can't be 0 or greater than 100"
-  validates_inclusion_of :ideal_staff_women, :in=>0..100, :message => "can't be less than 0 or greater than 100"
+  # validates_inclusion_of :ideal_staff_men, :in=>1..100, :message => "can't be 0 or greater than 100"
+  # validates_inclusion_of :ideal_staff_women, :in=>0..100, :message => "can't be less than 0 or greater than 100"
   
   validates_uniqueness_of :name
   validate :validate_partnership
