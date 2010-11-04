95835633c31ad4a3a8c405e543cd45dedd032ae0
Josh Starcher <josh.starcher@gmail.com>
2010-11-04 17:21:22 -0400

    password reset functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -41,4 +41,17 @@ class SessionsController < ApplicationController
     end
   end
   
+  def send_password_email
+    if params[:email].present?
+      @user = User.where(:username => params[:email]).first
+      if @user
+        @user.generate_password_key!
+        SessionMailer.password_link(@user).deliver
+        redirect_to login_path(:username => params[:email]), :notice => "Password reset email sent. If you don't see it within 2 minutes, please check your spam folder and add help@campuscrusadeforchrist.com to your spam filter."
+      else
+        redirect_to :back, :error => "Couldn't find a record with that email."
+      end
+      return
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails::Application.routes.draw do
   match '/auth/:provider/callback' => 'authentications#create'
   match '/auth/failure' => 'authentications#failed'
   resources :authentications
-  resource :session
-  resources :users
+  resource :session do
+    get :send_password_email
+  end
+  resources :users do
+    collection do
+      get :reset_password
+    end
+  end
   match '/login' => "sessions#new", :as => :login
   match '/logout' => "sessions#destroy", :as => :logout
   
