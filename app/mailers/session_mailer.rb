c97e765fd4850064eb2d5ddaf40088628945bae5
Josh Starcher <josh.starcher@gmail.com>
2010-11-05 12:31:12 -0400

    new mailer api for rails3

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -2,8 +2,8 @@ class SessionMailer < ActionMailer::Base
   default :from => "Summer Projects <gosummerproject@uscm.org>"
   
   def password_link(user)
-    recipients user.username
     subject "Campus Crusade Password Reset"
     @user = user
+    mail(:to => user.username, :subject => subject) 
   end
 end
