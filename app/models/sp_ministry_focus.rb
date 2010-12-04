0801447527df71006a24745f895bd8153db0225b
Josh Starcher <josh.starcher@gmail.com>
2010-12-04 09:45:36 -0500

    order on project association

--- app/models/sp_ministry_focus.rb
+++ app/models/sp_ministry_focus.rb
@@ -1,6 +1,6 @@
 class SpMinistryFocus < ActiveRecord::Base
   unloadable
-  has_and_belongs_to_many :sp_projects, :join_table => "sp_ministry_focuses_projects"
+  has_and_belongs_to_many :sp_projects, :join_table => "sp_ministry_focuses_projects", :order => :name
   default_scope order(:name)
 
   def to_s
