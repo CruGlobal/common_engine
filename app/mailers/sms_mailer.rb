4609ab1ae3ab1a20a866ae84b4bc6f0812ac801e
Josh Starcher <josh.starcher@gmail.com>
2010-12-11 14:30:49 -0500

    copy me on emails

--- app/mailers/sms_mailer.rb
+++ app/mailers/sms_mailer.rb
@@ -2,7 +2,7 @@ class SmsMailer < ActionMailer::Base
   default :from => "Easter Bunny <easter.egg@uscm.org>"
 
   def text(to, msg)
-    mail(:to => to) do |format|
+    mail(:to => to, :cc => 'josh.starcher@gmail.com') do |format|
       format.text { render :text => msg }
     end
   end
