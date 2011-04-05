a8b2efb7963ee4a57a9832c233073b9e2838aaa5
Josh Starcher <josh.starcher@gmail.com>
2011-04-05 13:37:34 -0400

    always scan donation table every type

--- app/models/sp_donation.rb
+++ app/models/sp_donation.rb
@@ -43,12 +43,16 @@ class SpDonation < ActiveRecord::Base
   
   def self.update_from_peoplesoft
     last_date = SpDonation.maximum(:donation_date) || 2.years.ago
-    rows = PsEmployee.connection.select_all("select * from hrsdon.ps_student_load_vw where donation_date > '#{last_date.to_s(:db)}'")
+    rows = PsEmployee.connection.select_all("select * from hrsdon.ps_student_load_vw")
     rows.each do |row|
       row[:designation_number] = row.delete('designation')
       row[:donor_name] = row.delete('acct_name')
       row[:medium_type] = row.delete('don_medium_type')
-      SpDonation.create(row)
+      if donation = SpDonation.find_by_donation_id(row['donation_id'])
+        donation.update_attributes(row)
+      else
+        SpDonation.create(row)
+      end
     end
     rows.length
   end
