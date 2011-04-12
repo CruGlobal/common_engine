ed08dd72e170fa308155d3805c4d866fab55e748
Josh Starcher <josh.starcher@gmail.com>
2011-04-12 09:28:51 -0400

    wrap the donations in a transaction

--- app/models/sp_donation.rb
+++ app/models/sp_donation.rb
@@ -44,14 +44,16 @@ class SpDonation < ActiveRecord::Base
   def self.update_from_peoplesoft
     last_date = SpDonation.maximum(:donation_date) || 2.years.ago
     rows = PsEmployee.connection.select_all("select * from hrsdon.ps_student_load_vw")
-    rows.each do |row|
-      row[:designation_number] = row.delete('designation')
-      row[:donor_name] = row.delete('acct_name')
-      row[:medium_type] = row.delete('don_medium_type')
-      if donation = SpDonation.find_by_donation_id(row['donation_id'])
-        donation.update_attributes(row)
-      else
-        SpDonation.create(row)
+    SpDonation.transaction do
+      rows.each do |row|
+        row[:designation_number] = row.delete('designation')
+        row[:donor_name] = row.delete('acct_name')
+        row[:medium_type] = row.delete('don_medium_type')
+        if donation = SpDonation.find_by_donation_id(row['donation_id'])
+          donation.update_attributes(row)
+        else
+          SpDonation.create(row)
+        end
       end
     end
     rows.length
