1ea2b13a030d8e180f34235e71bb5ce180fe5352
Josh Starcher <josh.starcher@gmail.com>
2011-04-20 17:12:56 -0400

    transaction might be to blame for duplicates?

--- app/models/sp_donation.rb
+++ app/models/sp_donation.rb
@@ -44,16 +44,14 @@ class SpDonation < ActiveRecord::Base
   def self.update_from_peoplesoft
     last_date = SpDonation.maximum(:donation_date) || 2.years.ago
     rows = PsEmployee.connection.select_all("select * from hrsdon.ps_student_load_vw")
-    SpDonation.transaction do
-      rows.each do |row|
-        row[:designation_number] = row.delete('designation')
-        row[:donor_name] = row.delete('acct_name')
-        row[:medium_type] = row.delete('don_medium_type')
-        if donation = SpDonation.find_by_donation_id(row['donation_id'])
-          donation.update_attributes(row)
-        else
-          SpDonation.create(row)
-        end
+    rows.each do |row|
+      row[:designation_number] = row.delete('designation')
+      row[:donor_name] = row.delete('acct_name')
+      row[:medium_type] = row.delete('don_medium_type')
+      if donation = SpDonation.find_by_donation_id(row['donation_id'])
+        donation.update_attributes(row)
+      else
+        SpDonation.create(row)
       end
     end
     rows.length
