f29f4bf5aa775a4a79c8c7855f0fc26938fe37f1
Josh Starcher <josh.starcher@gmail.com>
2011-06-08 10:56:02 -0400

    delete the past year of donations before inserting new ones

--- app/models/sp_donation.rb
+++ app/models/sp_donation.rb
@@ -42,16 +42,19 @@ class SpDonation < ActiveRecord::Base
   end
   
   def self.update_from_peoplesoft
-    last_date = SpDonation.maximum(:donation_date) || 2.years.ago
+    # last_date = SpDonation.maximum(:donation_date) || 2.years.ago
     rows = PsEmployee.connection.select_all("select * from hrsdon.ps_student_load_vw")
-    rows.each do |row|
-      row[:designation_number] = row.delete('designation')
-      row[:donor_name] = row.delete('acct_name')
-      row[:medium_type] = row.delete('don_medium_type')
-      if donation = SpDonation.find_by_donation_id(row['donation_id'])
-        donation.update_attributes(row)
-      else
-        SpDonation.create(row)
+    SpDonation.delete_all(["donation_date >  ?", 1.year.ago])
+    SpDonation.transaction do
+      rows.each do |row|
+        row[:designation_number] = row.delete('designation')
+        row[:donor_name] = row.delete('acct_name')
+        row[:medium_type] = row.delete('don_medium_type')
+        if donation = SpDonation.find_by_donation_id(row['donation_id'])
+          donation.update_attributes(row)
+        else
+          SpDonation.create(row)
+        end
       end
     end
     rows.length
