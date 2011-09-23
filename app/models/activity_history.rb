bc409a98fd91ad0cfcb0dc6c484eb5255e274750
Justin Sabelko <justin.sabelko@uscm.org>
2011-09-22 21:01:41 -0500

    Add methods

--- app/models/activity_history.rb
+++ app/models/activity_history.rb
@@ -2,4 +2,15 @@ class ActivityHistory < ActiveRecord::Base
   unloadable
   set_table_name "ministry_activity_history"
   belongs_to :activity
+  
+  def self.max_records_for_date(date)
+    where(ActivityHistory.table_name + ".period_begin <= ?", date).
+    group(ActivityHistory.table_name + ".activity_id").having("max(" + ActivityHistory.table_name + ".period_begin)")
+  end
+  
+  def self.max_date_for_date_activity(date, activity_id)
+    where(ActivityHistory.table_name + ".activity_id = ?", activity_id).
+    where(ActivityHistory.table_name + ".period_begin <= ?", date).
+    group(ActivityHistory.table_name + ".activity_id").maximum(ActivityHistory.table_name + ".period_begin")
+  end 
 end
