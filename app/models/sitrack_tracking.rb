bec6ec9dea9bcab7b7c0afe0fc70e2c8535ba33e
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-04-30 18:44:26 +0800

    Add attr_accessibles

--- app/models/sitrack_tracking.rb
+++ app/models/sitrack_tracking.rb
@@ -78,6 +78,7 @@ class SitrackTracking < ActiveRecord::Base
   self.table_name = "sitrack_tracking"
   belongs_to                :hr_si_application, :foreign_key => 'application_id'
   
+  attr_accessible :application_id
   def is_stint?
     return true if ['ICS','STINT'].include?(internType)
     return false
