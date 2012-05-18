6dc8f23ae256691dfc6e26b5182ea127db2ef60b
Josh Starcher <josh.starcher@gmail.com>
2012-05-18 09:41:22 -0400

    more attributes

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,7 +5,7 @@ class Person < ActiveRecord::Base
   self.primary_key = "personID"
   
   attr_accessible :firstName, :lastName, :isSecure, :birth_date, :maritalStatus,
-                  :accountNo, :first_name, :last_name, :gender
+                  :accountNo, :first_name, :last_name, :gender, :preferredName, :current_address_attributes
   
   # SP-298
   has_many                :sp_designation_numbers
