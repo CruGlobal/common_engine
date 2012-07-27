987adb3d3a4135140e31a78b309cae89d2e777fc
Justin Sabelko <justin.sabelko@uscm.org>
2012-07-27 12:54:13 -0500

    No need for password anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,11 +19,11 @@ class User < ActiveRecord::Base
   attr_accessor :plain_password, :plain_password_confirmation
 
   validates_format_of       :username, :message => "must be an email address", :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
-  validates_presence_of     :plain_password,                   :if => :password_required?
-  validates_presence_of     :plain_password_confirmation,      :if => :password_required?
-  validates_length_of       :plain_password, :within => 6..80, :if => :password_required?
+  #validates_presence_of     :plain_password,                   :if => :password_required?
+  #validates_presence_of     :plain_password_confirmation,      :if => :password_required?
+  #validates_length_of       :plain_password, :within => 6..80, :if => :password_required?
   # validates_format_of       :plain_password, :message => "isn't secure enough (you must include upper and lower case letters)", :with => /[a-z]+.*[A-Z]+|[A-Z]+.*[a-z]+/, :if => :password_required?
-  validates_confirmation_of :plain_password,                   :if => :password_required?
+  #validates_confirmation_of :plain_password,                   :if => :password_required?
   # validates_presence_of     :secret_question,                  :if => :password_required?
   # validates_presence_of     :secret_answer,                    :if => :password_required?
   validates_uniqueness_of   :username, :case_sensitive => false, :message => "is already registered in our system.  This may have occurred when you registered for a Campus Crusade related conference; therefore, you do not need to create a new account. If you need help with your password, please click on the appropriate link at the login screen.  If you still need assistance, please send an email to help@campuscrusadeforchrist.com describing your problem."
