2cb5a24dfc13a0e5a4d0cb0c7f7148f12a70cfa9
Justin Sabelko <justin.sabelko@uscm.org>
2012-07-30 10:55:29 -0500

    Make CurrentAddress behave the way it was intended to I believe (for saving at least)

--- app/models/current_address.rb
+++ app/models/current_address.rb
@@ -1,4 +1,14 @@
 class CurrentAddress < Address
   unloadable
   attr_accessible :address1, :address2, :city, :state, :zip, :homePhone, :cellPhone
+  
+  def save(*)
+    self.addressType = "current"
+    super
+  end
+
+  def save!(*)
+    self.addressType = "current"
+    super
+  end
 end
