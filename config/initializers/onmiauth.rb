4ee3716e1a45f21ec4f7f9947229df82dcbd26c1
Justin Sabelko <justin.sabelko@uscm.org>
2012-10-08 16:24:33 -0500

    google apps is no longer a login strategy

--- config/initializers/onmiauth.rb
+++ config/initializers/onmiauth.rb
@@ -8,7 +8,7 @@ if defined?(OmniAuth::Builder)
     if defined?(FACEBOOK)
       provider :facebook, FACEBOOK[:api_key], FACEBOOK[:secret_key], {:scope => 'user_about_me,user_birthday,email,offline_access'}
     end
-    provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :domain => 'gmail.com'
+    #provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :domain => 'gmail.com'
     provider :CAS, :host => 'https://signin.ccci.org/cas', :name => 'relay'
 
     # provider :open_id, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
