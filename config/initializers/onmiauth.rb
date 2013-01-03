cfc6de03ec42e4c29dce1d42afe41867c21a7ea3
Josh Starcher <josh.starcher@gmail.com>
2013-01-03 14:40:01 -0500

    Remove references to openid

--- config/initializers/onmiauth.rb
+++ config/initializers/onmiauth.rb
@@ -3,7 +3,6 @@ if File.exist?(Rails.root.join("config/facebook.yml")) && !defined?(FACEBOOK)
   FACEBOOK = YAML.load(raw_config)[Rails.env].symbolize_keys
 end
 if defined?(OmniAuth::Builder)
-  require 'openid/store/filesystem'
   Rails.application.config.middleware.use OmniAuth::Builder do
     if defined?(FACEBOOK)
       provider :facebook, FACEBOOK[:api_key], FACEBOOK[:secret_key], {:scope => 'user_about_me,user_birthday,email,offline_access'}
@@ -11,7 +10,6 @@ if defined?(OmniAuth::Builder)
     #provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :domain => 'gmail.com'
     provider :CAS, :host => 'https://signin.ccci.org/cas', :name => 'relay'
 
-    # provider :open_id, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
   end
   OmniAuth.config.logger = Rails.logger
 end
