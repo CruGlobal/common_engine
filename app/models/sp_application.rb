2ccd5e1d4dd52f4d1171ce720a891be793626a19
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2013-02-19 23:52:31 +0800

    Use EST timezone when generating application deadlines

--- app/models/sp_application.rb
+++ app/models/sp_application.rb
@@ -200,15 +200,15 @@ class SpApplication < AnswerSheet
   end
 
   def self.deadline1
-    Time.parse((SpApplication.year - 1).to_s + "/12/10")
+    Time.parse((SpApplication.year - 1).to_s + "/12/10 00:00:00 EST")
   end
 
   def self.deadline2
-    Time.parse(SpApplication.year.to_s + "/01/24")
+    Time.parse(SpApplication.year.to_s + "/01/24 00:00:00 EST")
   end
 
   def self.deadline3
-    Time.parse(SpApplication.year.to_s + "/02/24")
+    Time.parse(SpApplication.year.to_s + "/02/24 00:00:00 EST")
   end
 
   def name
