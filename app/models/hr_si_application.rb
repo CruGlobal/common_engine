b56db63b7ce88d8e56e5829a9775afd78e6e03d8
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2013-03-25 17:04:06 +0800

    Add staff checker

--- app/models/hr_si_application.rb
+++ app/models/hr_si_application.rb
@@ -2,21 +2,21 @@ class HrSiApplication < ActiveRecord::Base
   unloadable
   self.table_name = "hr_si_applications"
   self.primary_key = "applicationID"
-  
+
   attr_accessible :ssn, :fk_personID
-  
+
   belongs_to :apply
   belongs_to :person, :foreign_key => "fk_personID"
   has_one    :sitrack_tracking, :foreign_key => 'application_id'
   has_one    :sitrack_mpd, :foreign_key => 'application_id'
   has_many   :sitrack_salary_forms
-  belongs_to :location_a, :foreign_key => "locationA", :class_name => "HrSiProject" 
-  belongs_to :location_b, :foreign_key => "locationB", :class_name => "HrSiProject" 
-  belongs_to :location_c, :foreign_key => "locationC", :class_name => "HrSiProject" 
-  
+  belongs_to :location_a, :foreign_key => "locationA", :class_name => "HrSiProject"
+  belongs_to :location_b, :foreign_key => "locationB", :class_name => "HrSiProject"
+  belongs_to :location_c, :foreign_key => "locationC", :class_name => "HrSiProject"
+
   before_create :create_apply
   before_save :stamp
-  
+
   YEAR = 2013
 
   #set dateChanged and changedBy
@@ -29,13 +29,18 @@ class HrSiApplication < ActiveRecord::Base
     return false
   end
 
+  def self.is_staff(type)
+    return true if 'Part Time Field Staff' == type
+    return false
+  end
+
   def find_or_create_apply()
     if self.apply.nil?
       create_apply
     end
     self.apply
   end
-  
+
 protected
   def create_apply
     self.dateAppStarted = Time.now
