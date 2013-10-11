24cbe8233f4969baffdce6181e980eaca9bd742f
Josh Starcher <josh.starcher@gmail.com>
2013-10-11 14:32:31 -0400

    Fix year thing

--- app/models/sp_application.rb
+++ app/models/sp_application.rb
@@ -208,7 +208,7 @@ class SpApplication < ActiveRecord::Base
 
   # When changing this method, make sure the commit gets pulled into the MPD Tool as well
   def self.year
-    Time.now.month >= 9 && Time.now.day >= 15 ? Time.now.year + 1 : Time.now.year
+    Time.now.month >= 9 ? Time.now.year + 1 : Time.now.year
   end
 
   def self.deadline1
