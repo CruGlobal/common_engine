0b81249c93d3fadc4ee159bcc3eb33106c1ada7d
Matthew Drees <matt.drees@cru.org>
2013-10-15 12:49:28 -0600

    adapt to wsapi designation service changes
    (and some slight refactoring & renaming)

--- app/models/sp_designation_number.rb
+++ app/models/sp_designation_number.rb
@@ -4,23 +4,28 @@ class SpDesignationNumber < ActiveRecord::Base
   belongs_to :project, :class_name => 'SpProject'
   has_many :donations, -> { order('donation_date desc') }, :class_name => "SpDonation", :primary_key => "designation_number"
 
-  after_create :secure_siebel
+  after_create :secure_designation_if_necessary
 
   def sp_application
     SpApplication.where(person_id: person_id, project_id: project_id, year: year).first
   end
 
   # if this person is going to a secure location, mark them as secure in siebel
-  def secure_siebel
-    if sp_application && sp_application.is_secure? && APP_CONFIG['siebel_url']
-      parameters = {designation: designation_number, startDate: "03-01-#{year}", endDate: "09-01-#{year}", key: APP_CONFIG['siebel_key'], dateFormatString: 'MM-dd-yyyy'}
+  def secure_designation_if_necessary
+
+    if sp_application && sp_application.is_secure? && APP_CONFIG['designation_base_url']
+      url = "#{APP_CONFIG['designation_base_url']}/designations/#{designation_number}/secureStatus"
+      parameters = {
+          startDate: "03-01-#{year}",
+          endDate: "09-01-#{year}",
+          access_token: APP_CONFIG['designation_access_token']}
       logger.ap parameters
-      RestClient::Request.execute(:method => :post, :url => APP_CONFIG['siebel_url'], :payload => parameters, :timeout => -1) { |res, request, result, &block|
+      RestClient::Request.execute(:method => :post, :url => url, :payload => parameters, :timeout => -1) { |res, request, result, &block|
         logger.ap res
         logger.ap request
         logger.ap result
                                             # check for error response
-                                            if res.code.to_i == 400
+                                            if res.code.to_i != 200
                                               raise res.inspect
                                             end
                                             res
