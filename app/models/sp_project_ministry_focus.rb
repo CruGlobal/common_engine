4dddf3dc339d5cb81628a1c3777bb8158a66bb1b
Josh Starcher <josh.starcher@gmail.com>
2013-10-17 11:33:06 -0400

    Global registry fix

--- app/models/sp_project_ministry_focus.rb
+++ app/models/sp_project_ministry_focus.rb
@@ -7,6 +7,13 @@ class SpProjectMinistryFocus < ActiveRecord::Base
   belongs_to :ministry_focus, :class_name => "SpMinistryFocus", :foreign_key => "ministry_focus_id"
   belongs_to :project, :class_name => "SpProject", :foreign_key => "project_id"
 
+  def async_push_to_global_registry
+    attributes_to_push['project_id'] = project.global_registry_id if project && project.global_registry_id
+    attributes_to_push['ministry_focus_id'] = ministry_focus.global_registry_id if ministry_focus && ministry_focus.global_registry_id
+
+    super
+  end
+
   def self.skip_fields_for_gr
     %w[id created_at updated_at global_registry_id]
   end
