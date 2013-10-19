cf06d8c634d8c02e094cf2bc225e382888069615
Weston Platter <westonplatter@gmail.com>
2013-10-19 12:23:13 -0600

    missing tables

---
+++ spec/models/wsn_application_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+# Mysql2::Error: Table 'ce_test.wsn_sp_wsnapplication' doesn't exist: SHOW FULL FIELDS FROM `wsn_sp_WsnApplication`
+
+describe WsnApplication do
+  # it { should belong_to :summer_project }
+  # it { should belong_to :person }
+end
