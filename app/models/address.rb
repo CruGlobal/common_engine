5a402268435ec9dd1f6caa4bf34693c948a4ff40
Josh Starcher <josh.starcher@gmail.com>
2013-11-20 08:37:46 -0500

    Add created_at and updated_at methods
    Apparently bad things happen when you try to use alias_attribute with
    created_at

--- app/models/address.rb
+++ app/models/address.rb
@@ -8,8 +8,8 @@ class Address < ActiveRecord::Base
 
 	before_save :stamp
 
-  #alias_attribute :created_at, :dateCreated
-  #alias_attribute :updated_at, :dateChanged
+  def updated_at() dateChanged end
+  def created_at() dateCreated end
 
 	def home_phone; homePhone; end
 	def cell_phone; cellPhone; end
