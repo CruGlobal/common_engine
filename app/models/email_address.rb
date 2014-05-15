113646a4e8e9e0440cafc01aa4026b9640f2f53d
Josh Starcher <josh.starcher@gmail.com>
2014-05-15 15:28:12 -0400

    Add global registry methods to email address

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,3 +1,13 @@
 class EmailAddress < ActiveRecord::Base
 	belongs_to :person
+
+  def async_push_to_global_registry(parent_id = nil)
+    parent_id = person.global_registry_id unless parent_id
+
+    super(parent_id)
+  end
+
+  def self.skip_fields_for_gr
+    %w[id created_at updated_at global_registry_id]
+  end
 end
