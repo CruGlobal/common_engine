92debec6c029f96603c32aaf269b8bfff3de7221
Josh Starcher <josh.starcher@gmail.com>
2014-05-23 09:20:05 -0400

    Automatically push data to the GR

--- lib/global_registry_measurement_methods.rb
+++ lib/global_registry_measurement_methods.rb
@@ -5,8 +5,8 @@ module GlobalRegistryMeasurementMethods
   extend ActiveSupport::Concern
 
   included do
-    #after_commit :push_to_global_registry
-    #after_destroy :delete_from_global_registry
+    after_commit :push_to_global_registry
+    after_destroy :delete_from_global_registry
   end
 
   def delete_from_global_registry
