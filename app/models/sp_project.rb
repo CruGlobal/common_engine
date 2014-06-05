0ae0cfbbb2127af435c5183a5352951071a8e53d
Justin Sabelko <justin.sabelko@cru.org>
2014-06-05 13:29:46 -0500

    Fix association

--- app/models/sp_project.rb
+++ app/models/sp_project.rb
@@ -34,7 +34,7 @@ class SpProject < ActiveRecord::Base
   #  image_accessor :logo
 
   # SP-298
-  has_many :sp_designation_numbers
+  has_many :sp_designation_numbers, foreign_key: :project_id
 
   belongs_to :created_by, :class_name => "::Person", :foreign_key => "created_by_id"
   belongs_to :updated_by, :class_name => "::Person", :foreign_key => "updated_by_id"
