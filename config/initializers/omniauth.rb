bf05856c1164eb4e33986df133cc60d8e6876f53
Justin Sabelko <justin.sabelko@cru.org>
2015-08-18 15:20:04 -0500

    Remove offline_access from FB scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ if defined?(OmniAuth::Builder)
   Rails.application.config.middleware.use OmniAuth::Builder do
     if defined?(FACEBOOK)
       provider :facebook, FACEBOOK[:api_key], FACEBOOK[:secret_key],
-               :scope => 'user_about_me,user_birthday,email,offline_access',
+               :scope => 'user_about_me,user_birthday,email',
                :client_options => {
                    :site => 'https://graph.facebook.com/v2.0',
                    :authorize_url => "https://www.facebook.com/v2.0/dialog/oauth"
