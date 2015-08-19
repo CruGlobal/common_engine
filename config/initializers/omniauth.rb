741678d0ddddaa2f2cbccec88a060ffba4e365c8
Justin Sabelko <justin.sabelko@cru.org>
2015-08-19 14:39:09 -0500

    Re-revert omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,17 +1,12 @@
-if File.exist?(Rails.root.join("config/facebook.yml")) && !defined?(FACEBOOK)
-  raw_config = File.read(Rails.root.join("config/facebook.yml"))
-  FACEBOOK = YAML.load(raw_config)[Rails.env].symbolize_keys
-end
 if defined?(OmniAuth::Builder)
   Rails.application.config.middleware.use OmniAuth::Builder do
-    if defined?(FACEBOOK)
-      provider :facebook, FACEBOOK[:api_key], FACEBOOK[:secret_key],
-               :scope => 'user_about_me,user_birthday,email',
-               :client_options => {
-                   :site => 'https://graph.facebook.com/v2.0',
-                   :authorize_url => "https://www.facebook.com/v2.0/dialog/oauth"
-               }
-    end
+    provider :facebook, ENV['FB_APP_ID'], ENV['FB_SECRET'],
+             :scope => 'user_about_me,user_birthday,email',
+             :client_options => {
+                 :site => 'https://graph.facebook.com/v2.0',
+                 :authorize_url => "https://www.facebook.com/v2.0/dialog/oauth"
+             }
+
     #provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :domain => 'gmail.com'
     provider :CAS, :host => 'https://signin.ccci.org/cas', :name => 'relay'
   end
